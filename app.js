8feca67af6025280452073735fed9970fa8bbd6f
linson <linson.lorance1@gmail.com>
2020-03-18 17:55:05 +0530

    FSJ0320B007

--- app.js
+++ app.js
@@ -1,31 +1,134 @@
 // Important Note - No Built-in functions to be used
 // Progression 1: Names and Input
+function strLength(s) {
+    var length = 0;
+    while (s[length] !== undefined)
+      length++;
+    return length;
+  }
+function checkVowels(s) {
+    var vcount = 0;
+    index=0;
+    while (s[index] !== undefined){
+    var alpha=s[index];
+    switch(alpha){
+        case "a":
+        case "A":
+        vcount++;    
+        console.log(" Vowelcount "+vcount+"\n");        
+        console.log(" Vowel "+alpha+" present in position "+index+"\n");
+        break;
+        case "e":
+        case "E":
+        vcount++;    
+        console.log(" Vowel "+alpha+" present in position "+index+"\n");
+        break;
+        case "i":
+        case "I":
+        vcount++;
+        console.log(" Vowel "+alpha+" present in position "+index+"\n");
+        break;
+        case "o":
+        case "O":
+        vcount++;
+        console.log(" Vowel "+alpha+" present in position "+index+"\n");
+        break;
+        case "u":
+        case "U":
+        vcount++;
+        console.log(" Vowel "+alpha+" present in position "+index+"\n");
+        break;     
+    }
+    index++;  
+  }
+    if(vcount==0){
+        console.log("\nNo vowels present\n");
+    }
+    
+  }
+
+ function checkCase(s){
+  var runner = 0;
+  var upperc=0;
+  var lowerc=0;
+  while (s[runner] !== undefined){
+    if(s[runner]>= 'A' && s[runner] <= 'Z'){
+      upperc++;
+     } else if (s[runner] >= 'a' && s[runner] <= 'z'){
+       lowerc++;
+     }
+
+    runner++;
+  }  
+ }
+ 
+ function printSep(s){
+  [...s].forEach(c => console.log(c))
+
+//   var text = 'uololooo';
+
+//  With ES6
+// [...text].forEach(c => console.log(c))
+
+// // With the `of` operator
+// for (const c of text) {
+//     console.log(c)
+// }
+
+//  With ES5
+// for (var x = 0, c=''; c = text.charAt(x); x++) { 
+//     console.log(c); 
+// }
+
+//  ES5 without the for loop:
+// text.split('').forEach(function(c) {
+//     console.log(c);
+// });
+
+ }
 
 // 1.1 Create a variable `ProGrad-1` with the driver's name.
+let ProGrad_1='XXXX';
 // 1.2 Print `"The driver's name is XXXX"`.
+console.log("The driver's name is "+ProGrad_1);
 // 1.3 Create a variable `ProGrad-2` with the navigator's name.
+let ProGrad_2='YYYY';
 // 1.4 Print `"The navigator's name is YYYY"`.
+console.log("The driver's name is "+ProGrad_2);
 
 // Progression 2: Control Statements - 1
 // 2.1. Depending on which name is longer, print:
 // - The driver has the longest name, it has XX characters. or
 // - It seems that the navigator has the longest name, it has XX characters. or
 // - Wow, you both have equally long names, XX characters!.
-
+let length_1=strLength(ProGrad_1);
+let length_2=strLength(ProGrad_2);
+if(length_1>length_2){
+console.log("The driver "+ProGrad_1+" has the longest string");
+}else if(length_2>length_1){
+console.log("The driver "+ProGrad_2+" has the longest string");
+}else{
+console.log("Wow, Both drivers have equally long names");
+}
 // 2.2. Check if the string contains vowels or not.
 // - If it contains vowels, print the name, and also print the vowel letters along with the vowel index. or
 // - print no vowels
 // - for example. In String ProGrad - o and a are vowels. Print ProGrad o a 2 5. 
+checkVowels(ProGrad_1);
+checkVowels(ProGrad_2);
 
 // 2.3. Check if the string contains uppercase and lowercase characters Xx
 // - Print the number of upper case characters
 // - Print the number of lower case characters
-
+checkCase(ProGrad_1);
 
 // Progression 3: Control Statements - 2
 // 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
-
+printSep(ProGrad_1);
 // 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
+for (i = (strLength(ProGrad_2) - 1); i >= 0; i--) {
+  document.write(ProGrad_2.charAt(i) + " ");
+}
 
 // 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
 // - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
